2784b173cc896ff6b68a315708a98427fa01a444
Gabriel Matte <gabrielandremarquez.matte@gmail.com>
2023-05-02 14:28:38 -0300

    Refatora função de validação de CPF, transforma tudo em static e adiciona gitignore

--- src/controller/CPFController.java
+++ src/controller/CPFController.java
@@ -1,75 +1,57 @@
 package controller;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
 public class CPFController {
-	
-	public CPFController() {
-		super();
-	}
-	
-	public void validaCPF(String CPF) {
+
+	public static void validaCPF(String CPF) {
 		int[] CpfArray = CPFtoArray(CPF);
-		
-		if (verificaCodigo(1, CpfArray) == true) {
+
+		if (verificaCodigo(CpfArray) == true) {
 			JOptionPane.showMessageDialog(null, "O CPF é válido");
 		} else {
 			JOptionPane.showMessageDialog(null, "O CPF é inválido");
 		}
 	}
-	
-//	Transforma o CPF em um vetor
-	private int[] CPFtoArray(String CPF) {
-		int[] CpfArray = new int[11];
-		for(int i = 0; i <= 10; i++) {
-			CpfArray[i] = Integer.parseInt(String.valueOf(CPF.charAt(i)));
+
+	// Transforma o CPF em um vetor
+	private static int[] CPFtoArray(String CPF) {
+		int[] CpfArray = new int[Math.min(CPF.length(), 11)];
+		for (int i = 0; i < CpfArray.length; i++) {
+			CpfArray[i] = Integer.parseInt(CPF.substring(i, i + 1));
 		}
 		return CpfArray;
 	}
-	
-//	Verifica o Codigo
-	private boolean verificaCodigo(int posicaoCodigo, int[] Cpf) {
-		int j = 0;
-		if (posicaoCodigo == 1) {
-			j = 10;
-		} else {
-			j = 11;
+
+	// Verifica o Codigo
+	private static boolean verificaCodigo(int[] Cpf) {
+		int soma = 0;
+		for (int i = 0; i < 9; i++) {
+			soma += Cpf[i] * (10 - i);
 		}
-		
-		
-		int indexParameter = 7 + posicaoCodigo;
-		
-		int resultado = 0;
-		for (int i = 0 ; i <= indexParameter; i++) {
-			resultado += j * Cpf[i];
-			j--;
-		}
-		
-		int restoDiv = resultado % 11;
-		if (restoDiv < 2) {
-			if (Cpf[indexParameter + 1] == 0) {
-				if (posicaoCodigo == 1) {
-					return verificaCodigo(2, Cpf);
-				} else {
-					return true;
-				}
-			} else {
-				return false;
-			}
+		int resto = soma % 11;
+		int primeiro_digito;
+		if (resto < 2) {
+			primeiro_digito = 0;
 		} else {
-			int dif = 11 - restoDiv;
-			if (Cpf[indexParameter + 1] == dif) {
-				if (posicaoCodigo == 1) {
-					return verificaCodigo(2, Cpf);
-				} else {
-					return true;
-				}
-			} else {
-				return false;
-			}
+			primeiro_digito = 11 - resto;
+		}
+		soma = 0;
+		for (int i = 0; i < 9; i++) {
+			soma += Cpf[i] * (11 - i);
+		}
+		soma += primeiro_digito * 2;
+		resto = soma % 11;
+		int segundo_digito;
+		if (resto < 2) {
+			segundo_digito = 0;
+		} else {
+			segundo_digito = 11 - resto;
+		}
+		if (primeiro_digito == Cpf[9] && segundo_digito == Cpf[10]) {
+			return true;
+		} else {
+			return false;
 		}
 	}
-	
-	
-	
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,16 +1,12 @@
 package view;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
 import controller.CPFController;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		
-		CPFController CPFcont = new CPFController();
-		
 		String CPF = JOptionPane.showInputDialog("Insira o seu CPF, sem pontos e hífen, para que possamos validá-lo");
-		CPFcont.validaCPF(CPF);
-		
+		CPFController.validaCPF(CPF);
 	}
 }
